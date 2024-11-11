9f3822b16468b34c8f8ded00fcc320d4898e21a9
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 20:08:07 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include <string.h> //strlen
+#include <stdlib.h> //atoi
+#include <math.h>
+
+int main()
+{
+	int a,i;
+	scanf("%d", &a);
+	printf("%d", a/5+a/25+a/125);
+
+	return 0;
+}
+
