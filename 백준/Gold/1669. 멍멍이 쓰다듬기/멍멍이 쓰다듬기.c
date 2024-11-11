11cfd740448dcfbefc2872f5f4fda6fa06a8266d
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 20:08:01 +0900

    [Gold V] Title: 멍멍이 쓰다듬기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Gold/1669. 멍멍이 쓰다듬기/멍멍이 쓰다듬기.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	long long int T, x, y, d, j = 2, c;
+	scanf("%lld %lld", &x, &y);
+	d = y - x;
+	j = 2;
+	while (1)
+	{
+		if (d == 0 || d == 1 || d == 2)
+		{
+			c = d;
+			break;
+		}
+		else if (j * (j - 1) < d && d <= j * j)
+		{
+			c = 2 * j - 1;
+			break;
+		}
+		else if (j * j < d && d <= j * (j + 1))
+		{
+			c = 2 * j;
+			break;
+		}
+		j++;
+	}
+	printf("%lld\n", c);
+	return 0;
+}
