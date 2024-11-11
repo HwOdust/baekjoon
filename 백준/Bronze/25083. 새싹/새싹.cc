9bf1e915ebbc24310b5563651e96c72de31a016b
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 21:07:13 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <string.h> //strlen
+#include <stdlib.h> //atoi
+
+
+int main()
+{
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |");
+
+	 
+	return 0;
+}
