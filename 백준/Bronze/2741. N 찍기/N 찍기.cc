1bc742aa308ce4a5dfc4209bf5ff2cdf89914953
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 20:17:47 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <string.h> //strlen
+#include <stdlib.h> //atoi
+
+
+
+int main()
+{
+	int n,i;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
+
+
