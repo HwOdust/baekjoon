bf1e78a19fd6ea8142b8015d38725b5eb776cea3
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 20:40:19 +0900

    [Bronze IV] Title: Tabliczka, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/8674. Tabliczka/Tabliczka.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	long long a, b;
+	scanf("%lld %lld", &a, &b);
+	if (a % 2 == 0 || b % 2 == 0)
+		printf("0");
+	else printf("%lld", (a > b) ? b : a);
+	return 0;
+	
+}
