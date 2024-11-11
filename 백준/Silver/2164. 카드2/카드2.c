19ebfb2fae35f8102d0facbd7e9595fc5ee7af73
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 20:10:58 +0900

    [Silver IV] Title: 카드2, Time: 8 ms, Memory: 3064 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int arr[500001] = { 0 };
+int main()
+{
+	int n, cnt = 0,i,check=0;
+	scanf("%d", &n);
+	while(n!=1)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			if (arr[i] == 1)continue;
+			if (check == 0)
+			{
+				cnt++;
+				arr[i] = 1;
+				if (cnt == n - 1)break;
+				check=1;
+			}
+			else check = 0;
+		}
+		if (cnt == n - 1)break;
+	}
+	for (i = 1; i <= n; i++)
+	{
+		if (arr[i] == 0)printf("%d", i);
+	}
+	return 0;
+}
