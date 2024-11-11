1292cc15bf2d280ad855a5ab6acfe85cce12002b
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 21:09:52 +0900

    [Bronze III] Title: 사격 내기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27960. 사격 내기/사격 내기.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <string.h> //strlen
+#include <stdlib.h> //atoi
+#include <math.h>
+
+
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a ^ b);
+
+
+	return 0;
+}
+
+
+
+	
+	
