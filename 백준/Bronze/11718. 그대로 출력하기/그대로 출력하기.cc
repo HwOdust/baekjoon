14fc94687b09f8fe346dfd895b5fc37f4491f0c1
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 20:56:56 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <string.h> //strlen
+#include <stdlib.h> //atoi
+
+//https://blog.naver.com/moc24/222674508741
+
+
+int main()
+{
+	char a; 
+
+	while (scanf("%c", &a) != EOF)
+	{
+		
+		printf("%c", a);
+	}
+
+
+	return 0;
+}
