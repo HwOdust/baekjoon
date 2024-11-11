494ce10ebe3f2720415aa45cbd76760eacb47926
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 20:56:36 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 72 ms, Memory: 2676 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.c
@@ -0,0 +1,45 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+typedef struct //정보가 있는 구조체
+{
+	int x;
+	int y;
+}w;
+
+w chart[100000];
+
+int compare(const void* a, const void* b)
+{
+	w A = *(w*)a;
+	w B = *(w*)b;
+
+	if (A.y < B.y)return -1;
+	else if (A.y == B.y)
+	{
+		if (A.x < B.x)return -1;
+		else return 1;
+	}
+	else return 1;
+	return 0;
+}
+
+int main()
+{
+	int n, i;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d %d", &chart[i].x, &chart[i].y);
+	}
+
+	qsort(chart, n, sizeof(w*), compare);
+
+	
+	for (i = 0; i < n; i++)
+	{
+		printf("%d %d\n", chart[i].x, chart[i].y);
+	}
+	return 0;
+}
