6f7ae7d0a80cf7e07de1afefc427a883760d454d
Dust <168763026+HwOdust@users.noreply.github.com>
2024-11-11 21:09:27 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include <string.h> //strlen
+#include <stdlib.h> //atoi
+#include <math.h>
+
+int main() 
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a * b);
+	
+	return 0;
+}
