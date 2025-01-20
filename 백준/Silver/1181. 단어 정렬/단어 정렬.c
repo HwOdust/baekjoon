148970048a9f17679b23c613c0c13299e5a77c74
Dust <168763026+HwOdust@users.noreply.github.com>
2025-01-20 21:50:17 +0900

    [Silver V] Title: 단어 정렬, Time: 28 ms, Memory: 3108 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.c
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.c
@@ -1,33 +1,72 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include<string.h>
+#include <malloc.h>
 
-char word[20001][51] = { 0 };
+char tmp[20000][51] = { 0 };
+char arr[20000][51] = { 0 };
 
-int compare(const char* a, const char* b)
+void merge(int left, int right) //shit...
 {
-	if (strlen((char*)a) > strlen((char*)b))return 1;
-	else if (strlen((char*)a) < strlen((char*)b))return -1;
-	else return strcmp((char*)a, (char*) b);
+    int mid=(right + left) / 2;
+    int lp = left, rp = mid + 1, k = left;
 
+    while (lp <= mid && rp <= right)
+    {
+        if(strlen(arr[lp])<strlen(arr[rp]))strcpy(tmp[k++], arr[lp++]);
+        else if(strlen(arr[lp]) > strlen(arr[rp]))strcpy(tmp[k++], arr[rp++]);
+        else if (strcmp(arr[lp], arr[rp])<0)
+        {
+            strcpy(tmp[k++],arr[lp++]);
+        }
+        else
+        {
+            strcpy(tmp[k++], arr[rp++]);
+           // tmp[k++] = arr[rp++];
+        }
+    }
+    while (lp <= mid)
+    {
+        strcpy(tmp[k++], arr[lp++]);
+    }
+    while (rp <= right)
+    {
+        strcpy(tmp[k++], arr[rp++]);
+    }
+    for (int i = left; i <= right; i++)strcpy(arr[i],tmp[i]);
+}
+
+void partition(int left, int right) 
+{
+    if (left >= right) return;
+    int mid = (left + right) / 2;
+    partition(left, mid);
+    partition(mid + 1, right);
+    merge(left, right);
 }
 
 int main()
-{
-	int n, i;
-	scanf("%d", &n);
-	for (i = 0; i < n; i++)
-	{
-		scanf("%s", &word[i]);
-	}
+{    
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%s", &arr[i]);
+    }
 
-	qsort(word, n, sizeof(word[0]), compare);
-
-	
-	for (i = 0; i < n; i++)
-	{
-		if (strcmp(word[i],word[i-1])==0)continue;
-		else printf("%s\n", word[i]);
-	}
-	return 0;
+    partition(0, n-1);
+    int check = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0)
+        {
+            if (!strcmp(arr[i], arr[check]))
+            {
+                continue;
+            }
+        }
+        check = i;
+        printf("%s\n", arr[i]);
+    }
+    return 0;
 }
