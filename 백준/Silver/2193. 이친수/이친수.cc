4612bde5dfb381619fc2a9c2bf495497f2a791d7
Dust <168763026+HwOdust@users.noreply.github.com>
2025-07-25 17:33:20 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -3,8 +3,6 @@ using namespace std;
 long long dp[91]={0},n,i=3;
 int main()
 {
-    ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
     cin>>n;
     dp[1]=dp[2]=1;
     while(i<=n)dp[i++]=dp[i-1]+dp[i-2];
